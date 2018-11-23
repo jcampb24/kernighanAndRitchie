e346a4ba68a3d12f04ce4a0f4782c5ff078f50b5
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-11-22 20:46:59 -0600

    Switched from ints to floats for greater accuracy. Specified main return type and arguments to avoid complier warnings.

--- chapter_1/ex1.05.c
+++ chapter_1/ex1.05.c
@@ -3,9 +3,9 @@
 /* print Farhenheit-Celcius table
    for fahr = 300, 280, ..., 0 */
 
-main()
+int main(void)
 {
-  int fahr, celsius;
+  float fahr, celsius;
   int lower, upper, step;
 
   lower  = 0;    /* lower limit of temperature table. */
@@ -16,8 +16,8 @@ main()
   printf("Temperature Conversion Table\n");
   printf("Fahr\tCelsius\n");
   for(fahr=upper;fahr>=lower;fahr=fahr-step){
-    celsius=5*(fahr-32)/9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius=(5.0/9.0)*(fahr-32);
+    printf("%6.2f\t%6.2f\n", fahr, celsius);
   }
 
 }
