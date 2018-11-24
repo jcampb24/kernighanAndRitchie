afbc02c9cfe38fcb6debbab1e93df279b78bb4b4
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-11-23 19:40:56 -0600

    Modified initial stab to make output invariant to extra white space.

--- chapter_1/ex1.12.c
+++ chapter_1/ex1.12.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#define IN  1
+#define OUT 0
+//Program to copy its input with each word on a separate line.
+//Multiple white-space characters are ignored.
 
-main(){
+int main(void){
 
-  int c;
+  int c,state;
+  state=OUT; //Initialize the state to be out of a word.
   while((c=getchar())!=EOF){
-    putchar(c);
-    if(c== ' ' || c == '\t') putchar('\n');
+    if(c== ' ' || c == '\t' || c == '\n')//The current character is white space
+      {
+      if(state==IN) //We have encountered the end of a word.
+	{
+	  state=OUT;     //Switch the state to OUT, and
+	  putchar('\n'); //end the word with a newline character.
+	}
+      }
+    else //The current character is not white space.
+      {
+	if(state==OUT) state=IN; //Change the state to IN if required.
+	putchar(c);             //Put the current character into output.
+      }
   }
-
-
 }
