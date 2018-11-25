bbb0292768572cfe8a87d7f70375596be05d1d9c
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-11-24 18:17:03 -0600

    good working version of exercise 1.14, which accounts for ascii extensions without getting into to much detail.

--- chapter_1/ex1.14.c
+++ chapter_1/ex1.14.c
@@ -1,37 +1,54 @@
 #include <stdio.h>
 #define NUMCHARVALUES 128
+#define VISIBLE_MINIMUM 33
+#define VISIBLE_MAXIMUM 126
+
+//Creates a character histogram for extended-ASCII/UTF-8 input.
 int main(void){
 
-  int c,i,numchars,j;
-  int histogram[NUMCHARVALUES];
+  int c,i,j;
+  long numchars, numextends, histogram[NUMCHARVALUES];
 
   //Initialize the histogram;
   for(i=0;i<NUMCHARVALUES;i++) histogram[i]=0;
+  numextends=0;
 
   //Tabulate counts of each character.
-  while((c=getchar())!=EOF) histogram[c]++;
-  for(i=0;i<NUMCHARVALUES;i++) printf("%d\n",histogram[i]);
+  while((c=getchar())!=EOF) 
+    {
+      if(c<0){ 
+	printf("Error, negative character value: %d.",c); 
+	return(-1);
+      }
+      else if(c<NUMCHARVALUES) histogram[c]++;
+      else numextends++;
+    }
 
   //Calculate the total number of characters.
-  numchars=0;
+  numchars=numextends;
   for(i=0;i<NUMCHARVALUES;i++) numchars+=histogram[i];
-  printf("%d characters\n",numchars);
+
   //Report the histogram. For visible characters, place the character next to its histogram.
   printf("ASCII Code\tGlyph\t\tHistogram\n");
   for(i=0;i<NUMCHARVALUES;i++){
     if(histogram[i]>0){
       printf("%3d\t\t",i);
-      if(i>32 && i<127){
+      if(i>=VISIBLE_MINIMUM && i<=VISIBLE_MAXIMUM){
 	printf("%c\t\t",i);
       } else {
 	printf("NA\t\t");
       }
       c=100*histogram[i]/numchars;
-      printf("%d%%\t\t",c);
+      printf("%4.2f%%\t\t",100.0*histogram[i]/numchars);
       for(j=0;j<c;j++) printf("*");
       printf("\n");
     }
   }
+  printf("Extended\t\t\t");
+  printf("%4.2f%%\t\t",100.0*numextends/numchars);
+  c=100*numextends/numchars;
+  for(j=0;j<c;j++) printf("*");
+  printf("\n");
 
   return 0;
 
