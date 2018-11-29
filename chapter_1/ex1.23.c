5fb5411d2e02c17fad4041854b727ff45ea6db61
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-11-29 12:08:33 -0600

    Potential solution to exercise 23 compiles. Does not work properly.

--- chapter_1/ex1.23.c
+++ chapter_1/ex1.23.c
@@ -2,28 +2,79 @@
 #define IN 1
 #define OUT 0
 
+int startcomment(int[]);
+int endcomment(int[]);
+
 int main(void){
 
-  int inquotes, incomment;
+  int inquote, incomment;
   int lasttwochars[2];
-  int c;
+  int c,j,r;
 
-  for(c=0;c<2;c++) lasttwochars[c]='a'; /* Dummy initialization. */
+  for(j=0;j<2;j++) lasttwochars[j]=-1; /* Dummy initialization. */
   while((c=getchar())!=EOF){
 
-    /* If we are NOT in a comment and not in a quote, look to see if the current character starts a quote. */
-    if(incomment==OUT && inquote==OUT){
-      if(c=='"') inquote==IN;
+    /* Copy the current character into lasttwochars and shift its contents appropriately. */
+    lasttwochars[0]=lasttwochars[1];
+    lasttwochars[1]=c;
+
+    /* If we are not in a quote, see if the last two characters start a comment. If so, take appropriate action. */
+    if(inquote==OUT){
+      if(c=='"'){ //We are starting a quote. Change the state and feed the output stream.
+	inquote=IN;
+	if(lasttwochars[0]>=0) putchar(lasttwochars[0]);
+      }
+      else { //We are not starting a quote. See if we are starting a comment.
+	r=startcomment(lasttwochars);
+	if(r==1){ //A block comment has started. Search for its end.
+	  incomment=IN;
+	  while(incomment==IN){
+	    c=getchar();
+	    if(c==EOF) return(0); //The file ended with an open block comment. 
+	    lasttwochars[0]=lasttwochars[1];
+	    lasttwochars[1]=c;
+	    if(endcomment(lasttwochars)){//We found the comment's end. Switch the state and reset lasttwochars.
+	      incomment=OUT;
+	      for(j=0;j<2;j++) lasttwochars[j]=-1;
+	    }
+	  }
+	} else if (r==2) { //An inline comment has started. Search for its end.
+	  incomment=IN;
+	  while(incomment==IN){
+	    c=getchar();
+	    if(c==EOF) return(0); //The file ended with an open inline comment.
+	    if(c=='\n'){ //We have found the comment's end. Switch the state, Feed the newline to the output stream, and reset lasttwochars.
+	      incomment=OUT;
+	      putchar('\n');
+	      for(j=0;j<2;j++) lasttwochars[j]=-1;
+	    }
+	  }
+	} else if (r==0) { //We have not started a comment. Feed the previous character to output.
+	  if(c>=0) putchar(lasttwochars[0]);
+	} else { //startcomment gave an unknown return value. Return an error.
+	  printf("Error: startcomment returned %d.\n",r);
+	}
+      }
     }
     
-    /* If we are in a quote, look to see if the current character ends it. */
+    /* If we are in a quote, feed rthe output stream and look to see if the current character ends it. */
     if(inquote==IN){
-      if(c=='"') inquote==OUT;
+      lasttwochars[0]=lasttwochars[1];
+      lasttwochars[1]=c;
+      if(lasttwochars[0]>=0) putchar(lasttwochars[0]);
+      if(c=='"') inquote=OUT;
     }
-    
-
   }
+  return(0);
+}
 
+int startcomment(int twochars[]) {
+  if(twochars[0]=='/' && twochars[1]=='*') return(1);
+  if(twochars[0]=='/' && twochars[1]=='/') return(2);
+  return(0);
+}
 
-  return 0;
+int endcomment(int twochars[]) {
+  if(twochars[0]=='*' && twochars[1]=='/') return(1);
+  return(0);
 }
