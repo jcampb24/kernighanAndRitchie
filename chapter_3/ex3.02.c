1700640d4524d4150f152cd805b7b7d1b485a05e
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-12-02 14:48:32 -0600

    Exercise 3.2 completed.

--- chapter_3/ex3.02.c
+++ chapter_3/ex3.02.c
@@ -2,6 +2,7 @@
 #define MAX_LENGTH 100
 
 int escape(char s[], char t[]);
+int entrap(char t[], char s[]);
 
 int main(void){
 
@@ -19,6 +20,13 @@ int main(void){
   putchar('\n');
   printf("Escaped string:\n");
   for(i=0;t[i]!='\0';i++) putchar(t[i]);
+  putchar('\n');
+
+  //Feed the escaped string to the entrap function.
+  entrap(t,s);
+  //Report the entrapped output.
+  printf("Entrapped string:\n");
+  for(i=0;s[i]!='\0';i++) putchar(s[i]);
   putchar('\n');
 
 }
@@ -46,6 +54,44 @@ int escape(char s[], char t[]){
     }
   }
   t[j+1]='\0';
+
+  return(0);
+
+}
+
+int entrap(char t[], char s[]){
+
+  int i,j;
+  for(i=j=0;t[i]!='\0';i++){
+
+    switch(t[i]){
+
+    case '\\':
+      switch(t[i+1]){
+      case 'n':
+	s[j]='\n';
+	j++;
+	break;
+      case 't':
+	s[j]='\t';
+	j++;
+	break;
+      default:
+	s[j]='\\';
+	s[j+1]=t[i+1];
+	j=j+2;
+	break;
+      }
+      i++;
+      break;
+
+    default:
+      s[j]=t[i];
+      j++;
+      break;
+    }
+
+  }
 
   return(0);
 
