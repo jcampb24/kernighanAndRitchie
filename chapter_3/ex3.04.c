5bf78b4c8c340165cbcbf6c1e5acccfd69747a36
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-12-08 20:09:35 -0600

    Completed Exercise 3.4.

--- chapter_3/ex3.04.c
+++ chapter_3/ex3.04.c
@@ -41,12 +41,12 @@ void itoa(int n, char s[]){
 
   int i, sign;
 
-  if((sign=n)<0) 
+  if((sign=n)>0) 
     n = -n;
   i=0;
   do{
-    s[i++]= n % 10 + '0';
-  } while ((n/=10) > 0);
+    s[i++]= -(n % 10) + '0';
+  } while ((n/=10) < 0);
   if(sign < 0) 
     s[i++]='-';
   s[i]='\0';
