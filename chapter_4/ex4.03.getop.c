6cb33aadc263e55e43a3ea146915b4fd15a65185
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-12-15 16:44:14 -0600

    Added support for negative numbers to the calculator.

--- chapter_4/ex4.03.getop.c
+++ chapter_4/ex4.03.getop.c
@@ -1,4 +1,4 @@
-/* This file contains the code for the |getop| function, more or less as included in the text. */
+/* This file contains the code for the |getop| function, modified to complete Exercise 4.3. */
 #include <stdio.h>
 #include <ctype.h> //for |isdigit|.
 #define NUMBER '0'
@@ -13,9 +13,15 @@ int getop(char s[]){
   //Skip white space.
   while((s[0]=c=getch())==' ' || c == '\t');
 
-  s[1]='\0'; //By default, the string is empty.
-  if (!isdigit(c) && c!='.')
-    return(c); //c is not a number or a decimal point, so return it.
+  s[1]='\0'; // Prepare for the possibility that the input is complete.
+  if (!isdigit(c) && c!='.' && c!='-')
+    return(c); //c is not a number, a decimal point or a minus sign, so return it.
+  i=0;
+  if (c=='-'){ //If we stopped on a minus sign, process it correctly. It is the unary minus operator if immediately followed by a digit or by a decimal point. Otherwise, it is the binary subtraction operator.
+    c=getch();
+    if(!isdigit(c) && c!='.') return('-');
+    else s[++i]=c;
+  }
   if (isdigit(c)) //Collect any "integer" part of the number.
     while(isdigit(s[++i]=c=getch()));
   if (c=='.') //Collect any fractional part of the number.
