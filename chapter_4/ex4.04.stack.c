eb475b643f19cd119148c7e8c9d6922b5bbc332c
Jeffrey Campbell <J.R.Campbell@uvt.nl>
2018-12-15 17:46:49 -0600

    Solution to Exercise 4.4.

---
+++ chapter_4/ex4.04.stack.c
@@ -0,0 +1,43 @@
+/* ex4.04.stack.c
+This contains the expanded stack management functions used to complete Exercise 4.4. We add |peek| and |clear|. 
+*/
+#include<stdio.h>
+#define MAXVAL 100 // Maximum stack depth
+
+int sp=0;         //Next free stack position
+double val[MAXVAL];//The stack itself
+
+/* push: push |f| onto the stack. */
+void push(double f){
+
+  if(sp<MAXVAL)
+    val[sp++]=f; //The post-increment operator has its effect after |sp| is used as an index.
+  else
+    printf("error: Cannot push %g onto a full stack.\n",f);
+
+}
+
+double pop(void){
+
+  if(sp>0)
+    return val[--sp]; //The pre-decrement operator has its effect before |sp| is used as an index.
+  else{
+    printf("error: Cannot pop from an empty stack.\n");
+    return(0.0);
+  }
+}
+
+double peek(void){
+
+  if(sp>0){
+    return val[sp-1];
+  }  else {
+    printf("error: cannot peek into an empty stack.\n");
+    return(0.0);
+  }
+
+}
+
+void clear(void){
+  sp=0;
+}
